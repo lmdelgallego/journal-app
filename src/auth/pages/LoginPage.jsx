d04493d8ab33fb0dcea7d060bd6206b92ffc1de9
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-07 18:12:08 -0500

    ✨feature: 👷🏽‍♂️ 🎨 Style for AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,27 +1,11 @@
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
+import { AuthLayout } from "../layout/AuthLayout"
 
 const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction='column'
-      alignItems='center'
-      justifyContent='center'
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid item
-        className="box-shadow"
-        xs={3}
-        sx={{
-          backgroundColor: 'white',
-          padding: 3,
-          borderRadius: 2,
-        }}
-      >
-        <Typography variant="h5" sx={{ mb: 2}}>Login</Typography>
+    <AuthLayout title="Login">
         <form action="">
           <Grid container>
             <Grid item xs={12} sx={{ mb: 2}}>
@@ -47,8 +31,7 @@ const LoginPage = () => {
             </Grid>
           </Grid>
         </form>
-      </Grid>
-    </Grid>
+    </AuthLayout>
   )
 }
 
