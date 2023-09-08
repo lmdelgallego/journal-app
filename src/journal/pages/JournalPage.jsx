24638a778726139202b0df76d51465797d2e924a
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-08 13:26:04 -0500

    ✨feature: 👷🏽‍♂️ Creation NotView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,13 @@
 import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography variant='h1'>JournalPage</Typography> */}
-      <NothingSelectedView/>
+      {/* <NothingSelectedView/> */}
+      <NoteView/>
     </JournalLayout>
   )
 }
