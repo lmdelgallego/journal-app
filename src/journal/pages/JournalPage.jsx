d3512677684be2b6c907e3050ab21a562488f367
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-08 13:39:57 -0500

    ✨feature: 👷🏽‍♂️ Creation Button Add

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,29 @@
-import { Typography } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography variant='h1'>JournalPage</Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+
+      <IconButton
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.dark'
+          },
+          position: 'fixed',
+          bottom: 50,
+          right: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
