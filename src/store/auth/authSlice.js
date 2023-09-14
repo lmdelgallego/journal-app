94cc6794b4bda851dbea859acd2c1f77581f56d6
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-09-14 18:41:45 -0500

    ✨feature: 👷🏽‍♂️ Add auth store

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    status: 'checking', // 'checking' | 'authenticated' | 'not-authenticated',
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+  },
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    checkingCredentials: (state, action) => {
+    },
+    // setAuthStatus: (state, action) => {
+    //   state.status = action.payload;
+    // },
+    // setAuthUser: (state, action) => {
+    //   state.uid = action.payload.uid;
+    //   state.email = action.payload.email;
+    //   state.displayName = action.payload.displayName;
+    //   state.photoURL = action.payload.photoURL;
+    // },
+    // setAuthError: (state, action) => {
+    //   state.errorMessage = action.payload;
+    // },
+  }
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
+
+export default authSlice.reducer;
