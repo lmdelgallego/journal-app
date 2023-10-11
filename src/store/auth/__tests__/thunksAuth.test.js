bde1d6f230df38ff41211ff506b8c3d8d55a61db
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-10-11 08:00:17 -0500

    ✨feature(UnitTest): 👷🏽‍♂️ test for thunksAuth - startGoogleLogin

--- src/store/auth/__tests__/thunksAuth.test.js
+++ src/store/auth/__tests__/thunksAuth.test.js
@@ -1,6 +1,8 @@
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import { checkAuth } from "../thunks";
-import { checkingCredentials } from "../authSlice";
+import { checkAuth, startGoogleLogin } from "../thunks";
+import { checkingCredentials, login, logout } from "../authSlice";
+import { demoUser } from "./fixtures/authFixtures";
+import { singInWithGoogle } from "../../../firebase/providers";
 
 vi.mock("../../../firebase/providers")
 describe("thunksAuth", () => {
@@ -14,6 +16,32 @@ describe("thunksAuth", () => {
   test("should be invoque checkingCredentials", async () => {
     await checkAuth()(dispatch);
     expect(dispatch).toBeCalledWith(checkingCredentials());
+  });
+
+  test("should be invoque startGoogleLogin and login successfully", async () => {
+    const loginData = {
+      ok: true,
+      ...demoUser
+    }
+    await singInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleLogin()(dispatch);
+    expect(dispatch).toBeCalledTimes(2);
+    expect(dispatch).toBeCalledWith(checkingCredentials());
+    expect(dispatch).toBeCalledWith(login(loginData));
+  });
+
+  test("should be invoque startGoogleLogin and lout successfully with a error message", async () => {
+    const loginData = {
+      ok: false,
+      error:{
+        message: 'Google Error'
+      }
+    }
+    await singInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleLogin()(dispatch);
+    expect(dispatch).toBeCalledTimes(2);
+    expect(dispatch).toBeCalledWith(checkingCredentials());
+    expect(dispatch).toBeCalledWith(logout(loginData.error.message));
   });
 
 });
