d53ae0c05549bb7211951af487d4f7861969ec61
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-10-11 12:00:50 -0500

    ✨feature(UnitTest): 👷🏽‍♂️ finish test for thunksAuth

--- src/store/auth/__tests__/thunksAuth.test.js
+++ src/store/auth/__tests__/thunksAuth.test.js
@@ -1,8 +1,8 @@
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import { checkAuth, startGoogleLogin } from "../thunks";
+import { checkAuth, startCreateUser, startGoogleLogin, startLoginEmailPassword } from "../thunks";
 import { checkingCredentials, login, logout } from "../authSlice";
 import { demoUser } from "./fixtures/authFixtures";
-import { singInWithGoogle } from "../../../firebase/providers";
+import { loginWithEmail, singInWithGoogle, singUpWithEmail } from "../../../firebase/providers";
 
 vi.mock("../../../firebase/providers")
 describe("thunksAuth", () => {
@@ -43,5 +43,58 @@ describe("thunksAuth", () => {
     expect(dispatch).toBeCalledWith(checkingCredentials());
     expect(dispatch).toBeCalledWith(logout(loginData.error.message));
   });
+
+  describe("startCreateUser", () => {
+
+    test("should be invoque startCreateUser and login successfully", async () => {
+
+      await singUpWithEmail.mockResolvedValue({
+        ok: true,
+        ...demoUser
+      });
+      await startCreateUser(demoUser.email, demoUser.password, demoUser.displayName)(dispatch);
+      expect(dispatch).toBeCalledTimes(2);
+      expect(dispatch).toBeCalledWith(checkingCredentials());
+      expect(dispatch).toBeCalledWith(login({
+        ok: true,
+        ...demoUser
+      }));
+    });
+
+    test('should be invoque startCreateUser and logout with a error message', async () => {
+      await singUpWithEmail.mockResolvedValue({
+        ok: false,
+        errorMessage: 'Google Error'
+      });
+      await startCreateUser(demoUser.email, demoUser.password, demoUser.displayName)(dispatch);
+      expect(dispatch).toBeCalledTimes(2);
+      expect(dispatch).toBeCalledWith(checkingCredentials());
+      expect(dispatch).toBeCalledWith(logout({ errorMessage: 'Google Error'}));
+    });
+  });
+
+  describe('startLoginEmailPassword', () => {
+
+    test("should be invoque startLoginEmailPassword and login successfully", async () => {
+      const loginData = {
+        ok: true,
+        ...demoUser
+      };
+      await loginWithEmail.mockResolvedValue(loginData);
+      await startLoginEmailPassword(demoUser.email, demoUser.password)(dispatch);
+      expect(dispatch).toBeCalledTimes(2);
+    });
+
+    test('should be invoque startLoginEmailPassword and logout with a error message', async () => {
+      const loginData = {
+        ok: false,
+        errorMessage: 'Google Error'
+      };
+      await loginWithEmail.mockResolvedValue(loginData);
+      await startLoginEmailPassword(demoUser.email, demoUser.password)(dispatch);
+      expect(dispatch).toBeCalledTimes(2);
+      expect(dispatch).toBeCalledWith(logout({ errorMessage: 'Google Error'}));
+    });
+  })
 
 });
